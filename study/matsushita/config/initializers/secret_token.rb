73e07020b48ba8b1b1e74c73245adde8f6330767
MIZUNE Pine <pinemz@gmail.com>
2013-12-22 03:06:43 +0900

    add study rails project

---
+++ study/matsushita/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Matsushita::Application.config.secret_token = '00b27c53613cca5ec63962cc39d16967d44f3074826e66c1bba6e74981575d47c63f9f93a2a15bea618e8f983696d83efcc655fdf2ab56fef5c6f455099fc989'
