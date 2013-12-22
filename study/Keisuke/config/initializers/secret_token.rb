3687247dd36532c10bfc9bdbc032a6b6b2986371
K <sonicat7@gmail.com>
2013-12-22 21:19:43 +0900

    Keisuke new

---
+++ study/Keisuke/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Keisuke::Application.config.secret_token = 'e90f97626cbc6fa2d1f32b24b833af6a1fe127754593f8f69a62dcd7f6af5f237332a2c221e0045fcee7b4c7d3f2dc1bc85b9d1fc0185a4606316af43fc45dc2'
