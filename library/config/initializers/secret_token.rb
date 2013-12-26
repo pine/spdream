3c52aee2061b2fa4b6bf3e2aa8a6819247c80bc1
MIZUNE Pine <pinemz@gmail.com>
2013-12-26 16:50:43 +0900

    kut library web system $ rails new library

---
+++ library/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Library::Application.config.secret_token = '88a6fd92c05da4493990a2b79ed9524c8b12d0f38e25ad5c4000b868805c3e477526dc4eeaeb039429e05c67ef49597a4e10ec39a6105171891cc57f1e567fcd'
