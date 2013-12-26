a5d848996098588081b1b30e65287e90db9ac92c
K1reikirei <dkbup112@gmail.com>
2013-12-26 11:41:05 +0900

    tanaka is study blog

---
+++ study/tanaka/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'ff7e4e87b2de11f4844652c684c6d684b38099f230383be48fc636212dbaf7316c6be1000b7fbeb95f33c59346712356fbc699c6c034ffc98603190a37d585fd'
