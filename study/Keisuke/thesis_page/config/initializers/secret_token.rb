18cdd44a420779cb4146872eb6f9cb6abc7f9c0e
K <sonicat7@gmail.com>
2013-12-28 21:43:55 +0900

    削除と復元がうまく機能していません

--- study/Keisuke/thesis_page/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ThesisPage::Application.config.secret_token = '2b10488d68bb7bb1f518c32bb07e311302117d9ed1a2c9193ea8d86caa3e9692e8dc1bd3ec75117110bfb023d80eebfb916353f1bccc737b18b7c74580140afb'
--- study/Keisuke/thesis_page/test/unit/helpers/theses_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class ThesesHelperTest < ActionView::TestCase
-end
