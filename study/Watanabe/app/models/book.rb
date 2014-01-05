e621eb8c703534dae3c136a761d07c770108f17d
darkbcrow <g.d.b.crow@gmail.com>
2014-01-05 23:43:37 +0900

    OPAC+ 蔵書追加編集部分修正

--- study/Watanabe/app/models/book.rb
+++
@@ -1,3 +0,0 @@
-class Book < ActiveRecord::Base
-  attr_accessible :isbn10, :isbn13, :opac_id, :title, :value
-end
--- study/Watanabe/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Cms::Application.config.secret_token = '8bf28b92ff864225d59392f45ed06f5ec4340cc2c1c8252b51a0b1b34dabf043df463a4251ca33a2850b533e3533a34d780c88386afb9d5eca4f0d4cae3fec23'
