89d5c64e9814e94c1745373490f0c458ec901fdf
YOR <ace.of.diamond.1011@gmail.com>
2014-01-15 15:39:01 +0900

    View変更

--- study/ikezoe/config/application.rb
+++ study/ikezoe/config/application.rb
@@ -2,12 +2,18 @@ require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
 
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
 module Ikezoe
   class Application < Rails::Application
+
+
+    config.time_zone = 'Tokyo'
+ config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
