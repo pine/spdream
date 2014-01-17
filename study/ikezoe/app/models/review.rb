8976c7312c6026366ea99bdab752b68376d79ec8
YOR <ace.of.diamond.1011@gmail.com>
2014-01-17 12:17:14 +0900

    修正

--- study/ikezoe/app/models/review.rb
+++ study/ikezoe/app/models/review.rb
@@ -1,4 +1,4 @@
 class Review < ActiveRecord::Base
   belongs_to :book
-  attr_accessible :review, :book_id
+  attr_accessible :title, :book_id, :comment, :value_good, :value_bad, :value_book, :enabled, :date, :deleted
 end
