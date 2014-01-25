d6add074afcd2e37c2b9f15408285a5f57c6cdaf
darkbcrow <g.d.b.crow@gmail.com>
2014-01-25 16:05:28 +0900

    削除後移行場所変更

--- library/app/controllers/reviews_controller.rb
+++ library/app/controllers/reviews_controller.rb
@@ -81,7 +81,7 @@ class ReviewsController < ApplicationController
     @review.destroy
 
     respond_to do |format|
-      format.html { redirect_to reviews_url }
+      format.html { redirect_to :back }
       format.json { head :no_content }
     end
   end
